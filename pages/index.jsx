d796f7c3d2eb37c30fa68d660bf185a9ef22cda7
Vasko Nikolov <hugovasko1@gmail.com>
2023-06-11 22:44:04 +0300

    Project init

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
+};
+
+export const getStaticProps = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://hugovasko:vAMsVlfaWXitXX1m@nextcluster.1cfh4ti.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: (await meetups).map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+};
+
+export default HomePage;
